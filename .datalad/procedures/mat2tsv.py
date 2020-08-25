5c1e89a7dcc0b08f27010505d4666a5e309f653f
Judith Bomba <68907896+judithbomba@users.noreply.github.com>
2020-08-25 14:56:54 +0200

    Update mat2tsv.py
    should be ready for bids conversion now

--- .datalad/procedures/mat2tsv.py
+++ .datalad/procedures/mat2tsv.py
@@ -10,7 +10,7 @@ import sys
 import pandas as pd
 from datalad.distribution.dataset import require_dataset
 
-#apparently the call structure for python automatically takes the current dataset (root?) as positional argument number two
+#apparently the call structure for python automatically takes the current dataset (root?) as positional argument {ds} number two
 #to resolve said dataset and to make the procedure truly universally run-able this seems to be necessary
 ds = require_dataset(
     sys.argv[1],
@@ -18,7 +18,7 @@ ds = require_dataset(
     purpose='converting .mat to .tsv files')
 
 inputs = sys.argv[:]
-print('[HIRNI ADDONS] Running code with following inputs: ')
+print('[HIRNI ADDONS] Running mat2tsv converter with following inputs: ')
 print('[HIRNI ADDONS] ', inputs)
 #input1 = sys.argv[2] #my_source
 #input2 = sys.argv[3] #my_destination
@@ -27,46 +27,38 @@ location = sys.argv[2]
 bidsub = sys.argv[3]
 bidses = sys.argv[4]
 bidrun = sys.argv[5]
+func = 'func'
+head_tail = os.path.split(location)
+filename = head_tail[1]
+bids = os.path.join(ds,'sub-{}'.format(bidsub),'ses-{}'.format(bidses),func)
 
-
+ 
 print('[HIRNI ADDONS] Will gather .mat files from source:', location, 'will convert to .tsv files stored in:', input2)
 
 #print("the root of this project is:", os.path.dirname())
 
-root = os.path.dirname(os.path.abspath('source'))
-print("current working directory is: ",os.getcwd())
-print("var 'root' is set to: ", root)
+#root = os.path.dirname(os.path.abspath('source'))
+#print("current working directory is: ",os.getcwd())
+#print("var 'root' is set to: ", root)
 
-
-def mat_to_tsv(filename, filestring, destination):
+def mat_to_tsv(filelocation, filestring, destination):
     '''
-    Parameters
-    ----------
-    filename : os path
-        points to .mat files.
-    filestring : string
-        name of file being processed.
-    destination : os path
-        points to where .mat files should be stored.
-
-    Returns
-    -------
-    None.
+    based on https://gist.github.com/techedlaksh/9001039bf54ba9d8aec3ad7f5d8bfd08
 
     '''
     import pandas as pd
-    filenm = (filename)
+    file = (filelocation)
 
     try:
         import scipy.io
-        test = scipy.io.loadmat(filenm)
+        test = scipy.io.loadmat(file)
         #print(test)
     except NotImplementedError:
-        print("scipy is not working on this file")
+        print("[HIRNI ADDONS] ERROR: scipy is not working on this .mat file.")
     except:
-        ValueError('Warning: could not read the file at all...')
+        ValueError('[HIRNI ADDONS] WARNING: could not read the file at all...')
 
-    mat = scipy.io.loadmat(filenm)
+    mat = scipy.io.loadmat(file)
     mat = {k:v for k, v in mat.items() if k[0] != '_'}
     data = pd.DataFrame({k: pd.Series(v[0]) for k, v in mat.items()})
 
@@ -91,25 +83,25 @@ def mat_to_tsv(filename, filestring, destination):
     filename = filestring[:-4]
     dataframe.to_csv('{}/{}_events.tsv'.format(destination,filename), sep = '\t', index=False)
 
-
-
-filesource = os.path.join(root, input1)
+#filesource = os.path.join(root, input1)
 #print(filesource)
-destination = os.path.join(root, input2)
+#destination = os.path.join(root, input2)
 
 #print('#these are my files in root:\n',os.listdir(root))
 #print('#these are my files in {}:\n'.format(input1),os.listdir('{}/my_source'.format(root))) #my_s
-print('#these are my files in {} before converting:\n'.format(input2),os.listdir(destination)) #my_d
+#print('#these are my files in {} before converting:\n'.format(input2),os.listdir(destination)) #my_d
 #print(root)
 
-for file in os.listdir(filesource):
-    if file.endswith('.mat'):
-        filename = (os.path.join(filesource,file))
-        #print(filename)
-        filestring = file
-        print(filestring[:-4], "attempted to be processed")
-        mat_to_tsv(filename, filestring, destination)
+#for file in os.listdir(filesource):
+#    if file.endswith('.mat'):
+#        filename = (os.path.join(filesource,file))
+#        #print(filename)
+#        filestring = file
+#        print(filestring[:-4], "attempted to be processed")
+#        mat_to_tsv(filename, filestring, destination)
 
-print('#these are my files in {} after converting:\n'.format(input2),os.listdir(destination)) #my_d
+#print('#these are my files in {} after converting:\n'.format(input2),os.listdir(destination)) #my_d
 
-ds.save(path='.',message='convert .mat files from source ({}) to .tsv files and store them in ({})'.format(input1,input2))
+mat_to_tsv(location, filename, bids)
+ds.save(path='.',message='convert .mat file from source ({}) to .tsv files and store them in ({})'.format(location,bids))
+print('[HIRNI ADDONS] Converted .mat file from source ({}) to .tsv files and stored them in ({})'.format(location,bids))
